a6981576838c6638904e3ce1f9e47fac45d3763a
atenai <alfamod47@yahoo.co.jp>
2022-06-02 18:44:42 +0900

    1

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Advertisements;
+
+public class GameController : MonoBehaviour
+{
+
+    //private string gameIDGoogleplay = "4781103", gameIDAppstore = "4781102";
+    void Start()
+    {
+        //Advertisement.Initialize(gameIDGoogleplay, testMode: true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowAd()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+    }
+}
+
