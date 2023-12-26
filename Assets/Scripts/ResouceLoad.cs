136741e2f74b3f51253f4d97d1e74b87881b8b76
atenai <alfamod47@yahoo.co.jp>
2023-12-27 00:18:06 +0900

    画像のロード

---
+++ Assets/Scripts/ResouceLoad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ResouceLoad : MonoBehaviour
+{
+    void Start()
+    {
+        TestIconLoad();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void TestIconLoad()
+    {
+        UnityEngine.Texture tex = EditorGUIUtility.Load("Icon.png") as Texture;
+        Debug.Log(tex);
+    }
+}
