8e70b169e2d1e3782e6917da9c4a88a09237fb64
atenai <alfamod47@yahoo.co.jp>
2023-12-28 18:24:00 +0900

    Rangeのアトリビュート

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Range(1, 10)]
+    public int num1 = 1;
+
+    [Range(1, 10)]
+    public float num2 = 1;
+
+    [Range(1, 10)]
+    public long num3 = 1;
+
+    [Range(1, 10)]
+    public double num4 = 1;
+
+    void OnEnable()
+    {
+#if UNITY_EDITOR
+
+#endif
+    }
+}
