f0e01a4d2cff820df1c0241e7ea1f143afefe0d5
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2024-01-04 04:52:45 +0900

    エディタのサイズを追加

--- Assets/Scripts/ExampleWindow.cs
+++ Assets/Scripts/ExampleWindow.cs
@@ -8,6 +8,8 @@ public class ExampleWindow : EditorWindow
     int intervalTime = 60;
 
     const string AUTO_SAVE_INTERVAL_TIME = "AutoSaveinterval time (sec)";
+    const string SIZE_WIDTH_KEY = "ExampleWindow size width";
+    const string SIZE_HEIGHT_KEY = "ExampleWindow size height";
 
     [MenuItem("Window/Example")]//Unityエディタの上にあるWindowメニューに追加される
     static void Open()
@@ -18,6 +20,15 @@ public class ExampleWindow : EditorWindow
     void OnEnable()
     {
         intervalTime = EditorPrefs.GetInt(AUTO_SAVE_INTERVAL_TIME, 60);//ロード
+        var width = EditorPrefs.GetFloat(SIZE_WIDTH_KEY, 600);
+        var height = EditorPrefs.GetFloat(SIZE_HEIGHT_KEY, 400);
+        position = new Rect(position.x, position.y, width, height);
+    }
+
+    void OnDisable()
+    {
+        EditorPrefs.SetFloat(SIZE_WIDTH_KEY, position.width);
+        EditorPrefs.SetFloat(SIZE_HEIGHT_KEY, position.height);
     }
 
     void OnGUI()
