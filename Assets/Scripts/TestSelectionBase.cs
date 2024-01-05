ec1c7fa7383d4cde557c21fd5094472507edef90
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2024-01-06 02:53:48 +0900

    Jsonを作成

--- Assets/Scripts/TestSelectionBase.cs
+++ Assets/Scripts/TestSelectionBase.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 [SelectionBase]
 public class TestSelectionBase : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
 
--- Assets/Scripts/TweenColor.cs
+++ Assets/Scripts/TweenColor.cs
@@ -1,9 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using UnityEditor;
 using UnityEngine;
 
 [AddComponentMenu("MyUI/Tween Color")]
 public class TweenColor : MonoBehaviour
 {
+    void Start()
+    {
+        Debug.Log(JsonUtility.ToJson(new Example(), true));
+
+        var list = new List<Example>
+        {
+            new Example(),
+            new Example()
+        };
+
+        Debug.Log(JsonUtility.ToJson(list));
+    }
+
+    public static string ToJson(string key, Object[] objs)
+    {
+        var json = objs.Select(obj => EditorJsonUtility.ToJson(obj)).ToArray();
+        var values = string.Join(",", json);
+        return string.Format("{\"{0}\":{1}]}", key, values);
+    }
+
 
 }
